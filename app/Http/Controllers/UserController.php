d7ac4219b87af71210286be1b6563472e37eb95a
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2020-08-23 14:58:32 +0900

    #1 アイコン画像変更機能の実装

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
