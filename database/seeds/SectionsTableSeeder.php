28d4e49c66621a7a338c6c348f0efc4b2124b0e0
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2020-09-14 12:27:38 +0900

    #7 SectionsTableSeederの作成

---
+++ database/seeds/SectionsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Section;
+
+class SectionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i = 0; $i < 5; $i++) {
+            Section::insert([['room_id' => '1', 'role' => '自習']]);
+        }
+        for ($i = 0; $i < 5; $i++) {
+            Section::insert([['room_id' => '1', 'role' => '休憩']]);
+        }
+    }
+}
