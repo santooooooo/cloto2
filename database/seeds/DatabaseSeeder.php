3b1b076d7b3fd9e1cfd2b7413361aaef0c124923
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2020-09-14 12:49:15 +0900

    #7 SeatsTableSeederの作成

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(RoomsTableSeeder::class);
         $this->call(SectionsTableSeeder::class);
+        $this->call(SeatsTableSeeder::class);
     }
 }
