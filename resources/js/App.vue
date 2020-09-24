8cdc4b672de5640ed1e9d902a3a2e360faeb299e
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2020-09-25 00:35:52 +0900

    ヘッダー，ドロワー，フッターのコンポーネント化

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,107 +1,17 @@
 <template>
   <v-app>
     <!-- ヘッダー -->
-    <v-app-bar app dark>
-      <router-link :to="{ name: 'index' }">
-        <img :src="$storage('system') + 'header-logo.svg'" class="top-logo" />
-      </router-link>
+    <Header @show-drawer="isShowDrawer = true" />
 
-      <v-spacer></v-spacer>
-
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-    </v-app-bar>
-
-    <!-- ドロワーメニュー - 未ログイン時 -->
-    <v-navigation-drawer
-      app
-      dark
-      right
-      temporary
-      v-model="drawer"
-      v-if="!$store.getters['auth/check']"
-    >
-      <v-list nav>
-        <v-list-item :to="{ name: 'index' }">
-          <v-list-item-icon>
-            <v-icon>mdi-information</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>トップページ</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item :to="{ name: 'register' }">
-          <v-list-item-icon>
-            <v-icon>mdi-account-plus</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>新規登録</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item :to="{ name: 'login' }">
-          <v-list-item-icon>
-            <v-icon>mdi-login</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>ログイン</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-
-    <!-- ドロワーメニュー - ログイン時 -->
-    <v-navigation-drawer app dark right temporary v-model="drawer" v-else>
-      <v-list-item two-line class="px-3">
-        <v-list-item-avatar>
-          <img :src="$storage('icon') + $store.getters['auth/user'].icon" />
-        </v-list-item-avatar>
-
-        <v-list-item-content>
-          <v-list-item-title>{{ $store.getters['auth/user'].handlename }}</v-list-item-title>
-          <v-list-item-subtitle>{{
-            '@' + $store.getters['auth/user'].username
-          }}</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list nav>
-        <v-list-item :to="{ name: 'home' }">
-          <v-list-item-icon>
-            <v-icon>mdi-home-city</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>ホーム</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-
-        <v-list-item
-          :to="{ name: 'userPage', params: { username: $store.getters['auth/user'].username } }"
-        >
-          <v-list-item-icon>
-            <v-icon>mdi-account</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>マイページ</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-
-      <template v-slot:append v-if="$store.getters['auth/check']">
-        <div class="pa-2">
-          <v-btn block @click="logout">ログアウト</v-btn>
-        </div>
-      </template>
-    </v-navigation-drawer>
+    <!-- ドロワーメニュー -->
+    <Drawer v-model="isShowDrawer" @logout="logout" />
 
     <!-- メイン -->
     <!-- Sizes your content based upon application components -->
     <v-main>
       <!-- Provides the application the proper gutter -->
       <v-container fluid>
-        <div class="welcome__cloto-icon col-6" v-if="!$store.getters['auth/check']">
+        <div class="welcome__cloto-icon col-6" v-if="!authCheck">
           <img :src="$storage('system') + 'top.png'" />
         </div>
 
@@ -110,64 +20,35 @@
       </v-container>
     </v-main>
 
-    <!-- フッター - 未ログイン時 -->
-    <v-footer app dark absolute padless v-if="!$store.getters['auth/check']">
-      <v-row justify="center" no-gutters>
-        <v-btn color="white" text rounded class="my-2" :to="{ name: 'index' }">
-          トップページ
-        </v-btn>
-
-        <v-btn color="white" text rounded class="my-2" :to="{ name: 'register' }"> 新規登録 </v-btn>
-
-        <v-btn color="white" text rounded class="my-2" :to="{ name: 'login' }"> ログイン </v-btn>
-
-        <v-btn color="white" text rounded class="my-2" :to="'hoge'"> 利用規約 </v-btn>
-
-        <v-btn color="white" text rounded class="my-2" :to="'https://twitter.com/cloto_jp'">
-          Twitter
-        </v-btn>
-
-        <v-btn color="white" text rounded class="my-2" :to="'mailto:info@cloto.jp'"> E-Mail </v-btn>
-
-        <v-col dark class="py-4 text-center white--text" cols="12">
-          {{ new Date().getFullYear() }} — <strong>© CLOTO Team</strong>
-        </v-col>
-      </v-row>
-    </v-footer>
-
-    <!-- フッター - ログイン時 -->
-    <v-footer app dark absolute padless v-else>
-      <v-row justify="center" no-gutters>
-        <v-btn color="white" text rounded class="my-2" :to="{ name: 'home' }"> ホーム </v-btn>
-
-        <v-btn color="white" text rounded class="my-2" @click="logout"> ログアウト </v-btn>
-
-        <v-btn color="white" text rounded class="my-2" :to="'hoge'"> 利用規約 </v-btn>
-
-        <v-btn color="white" text rounded class="my-2" :to="'https://twitter.com/cloto_jp'">
-          Twitter
-        </v-btn>
-
-        <v-btn color="white" text rounded class="my-2" :to="'mailto:info@cloto.jp'"> E-Mail </v-btn>
-
-        <v-col dark class="py-4 text-center white--text" cols="12">
-          {{ new Date().getFullYear() }} — <strong>© CLOTO Team</strong>
-        </v-col>
-      </v-row>
-    </v-footer>
+    <!-- フッター -->
+    <Footer @logout="logout" />
   </v-app>
 </template>
 
 <script>
+import Header from './Header';
+import Drawer from './Drawer';
+import Footer from './Footer';
 import { UNAUTHORIZED, INTERNAL_SERVER_ERROR } from '@/consts/status';
 
 export default {
+  components: {
+    Header,
+    Drawer,
+    Footer,
+  },
   data() {
     return {
-      drawer: false, // ドロワーメニューの表示
+      isShowDrawer: false, // ドロワーメニューの表示
     };
   },
   computed: {
+    authCheck() {
+      return this.$store.getters['auth/check'];
+    },
+    authUser() {
+      return this.$store.getters['auth/user'];
+    },
     errorCode() {
       return this.$store.state.error.code;
     },
