6d3740512dbced87eb2886b84d0a37232f7a80c5
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2020-09-25 15:21:50 +0900

    カルテモーダルの削除

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -17,28 +17,6 @@
 
         <!-- If using vue-router -->
         <router-view></router-view>
-
-        <!-- カルテモーダル -->
-        <v-row justify="center" v-if="authCheck">
-          <v-btn fixed dark fab bottom right color="pink" @click="dialog = true">
-            <v-icon>mdi-pencil</v-icon>
-          </v-btn>
-
-          <v-dialog v-model="dialog" persistent max-width="800">
-            <v-card>
-              <v-card-title class="headline">Use Google's location service?</v-card-title>
-              <v-card-text
-                >Let Google help apps determine location. This means sending anonymous location data
-                to Google, even when no apps are running.</v-card-text
-              >
-              <v-card-actions>
-                <v-spacer></v-spacer>
-                <v-btn color="green darken-1" text @click="dialog = false">Disagree</v-btn>
-                <v-btn color="green darken-1" text @click="dialog = false">Agree</v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-dialog>
-        </v-row>
       </v-container>
     </v-main>
 
