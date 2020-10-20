5013f4e6acb6aa03dbdf4c900aa6cdd79361febc
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2020-10-20 22:15:00 +0900

    #13 休憩室のデザイン調整

--- resources/js/components/room/Lounge.vue
+++ resources/js/components/room/Lounge.vue
@@ -1,24 +1,66 @@
 <template>
-  <beautiful-chat
-    :open="enterLounge"
-    :close="leaveLounge"
-    :onMessageWasSent="onMessageWasSent"
-    :colors="chatColors"
-    :isOpen="isLoungeEnter"
-    :messageList="messageList"
-    :participants="chatParticipants"
-    :showCloseButton="true"
-    :showEmoji="true"
-    :showHeader="true"
-    :alwaysScrollToBottom="true"
-  >
-    <template v-slot:user-avatar="{ message, user }">
-      <div v-if="message.data.type === 'text' && user && user.name">
-        {{ user.name.toUpperCase()[0] }}
-      </div>
-    </template>
-    <template v-slot:system-message-body="{ message }"> [System]: {{ message.text }} </template>
-  </beautiful-chat>
+  <v-overlay z-index="4">
+    <v-layout>
+      <v-flex class="flex-column mr-12">
+        <v-col>
+          <v-avatar size="100">
+            <img :src="chatParticipants[0].imageUrl" />
+          </v-avatar>
+        </v-col>
+
+        <v-col>
+          <v-avatar size="100">
+            <img :src="chatParticipants[0].imageUrl" />
+          </v-avatar>
+        </v-col>
+      </v-flex>
+
+      <v-flex>
+        <beautiful-chat
+          :open="enterLounge"
+          :close="leaveLounge"
+          :onMessageWasSent="onMessageWasSent"
+          :colors="chatColors"
+          :isOpen="isLoungeEnter"
+          :messageList="messageList"
+          :participants="chatParticipants"
+          :showCloseButton="true"
+          :showEmoji="true"
+          :showHeader="false"
+          :alwaysScrollToBottom="true"
+        >
+          <template v-slot:user-avatar="{ message, user }">
+            <div v-if="message.data.type === 'text' && user && user.name">
+              {{ user.name.toUpperCase()[0] }}
+            </div>
+          </template>
+          <template v-slot:system-message-body="{ message }">
+            [System]: {{ message.text }}
+          </template>
+        </beautiful-chat>
+
+        <v-row justify="center">
+          <v-btn color="error" class="mt-12" @click="leaveLounge()" v-if="isLoungeEnter"
+            >自習室に戻る</v-btn
+          >
+        </v-row>
+      </v-flex>
+
+      <v-flex class="flex-column ml-12">
+        <v-col>
+          <v-avatar size="100">
+            <img :src="chatParticipants[0].imageUrl" />
+          </v-avatar>
+        </v-col>
+
+        <v-col>
+          <v-avatar size="100">
+            <img :src="chatParticipants[0].imageUrl" />
+          </v-avatar>
+        </v-col>
+      </v-flex>
+    </v-layout>
+  </v-overlay>
 </template>
 
 <script>
@@ -128,13 +170,24 @@ export default {
 
 <style lang="scss">
 /* beautiful-chatのスタイル */
-.sc-launcher {  
-  // モーダルオープンアイコンの無効化
+.sc-launcher {
+  // チャットオープンアイコンの無効化
   display: none;
 }
-.sc-chat-window[data-v-08d4c038]{
-position: absolute;
-margin-right:40vw;
-right: 0;
+
+.sc-chat-window {
+  // チャットの中央寄せ
+  position: static !important;
+
+  width: 60vw !important;
+
+  .sc-user-input {
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+  }
+
+  .sc-message {
+    width: 90%;
+  }
 }
 </style>
