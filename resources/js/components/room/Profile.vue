1a593903808edb21ab225a6ee9018a1e4d0d37ec
shutoushu <shutoushu@gmail.com>
2020-10-21 15:02:33 +0900

    #13 プロフィールモーダルのデザイン調整

--- resources/js/components/room/Profile.vue
+++ resources/js/components/room/Profile.vue
@@ -3,77 +3,18 @@
     <!-- ローディングバー -->
     <v-progress-linear indeterminate color="white" class="mb-0" v-if="!user"></v-progress-linear>
 
-    <v-card v-else>
+    <v-card color="grey darken-1" dark>
       <v-container>
+        <v-btn class="ma-auto" color="grey lighten-1" dark>
+          <v-icon dark @click="close()"> mdi-arrow-left </v-icon>
+        </v-btn>
         <v-row class="text-center" justify="center">
-          <v-col>
-            <v-avatar size="52"><img :src="$storage('icon') + user.icon" /></v-avatar>
-            <p>{{ user.handlename }}</p>
-            <div class="introduction">
-              <p>{{ user.introduction }}</p>
-            </div>
-          </v-col>
-          <v-col cols="3" sm="3">
-            <div v-if="sns || user.web">
-              <a :href="'https://twitter.com/' + sns.twitter" target="_blank" v-if="sns.twitter">
-                <i class="fab fa-twitter fa-2x"></i>
-              </a>
-              <a :href="'https://github.com/' + sns.github" target="_blank" v-if="sns.github">
-                <i class="fab fa-github fa-2x"></i>
-              </a>
-              <a :href="'https://qiita.com/' + sns.qiita" target="_blank" v-if="sns.qiita">
-                <i class="fa fa-search fa-2x"></i>
-              </a>
-              <a :href="user.web" target="_blank" v-if="user.web">
-                <i class="fas fa-link fa-2x"></i>
-              </a>
-            </div>
-          </v-col>
-        </v-row>
-
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="green darken-1" text @click="close()"> close </v-btn>
-        </v-card-actions>
-      </v-container>
-    </v-card>
-  </v-dialog>
-
-  <!-- <div class="profile"> -->
-  <!-- プロフィール欄 -->
-  <!-- <div class="profile__content card">
-        <div class="row"> -->
-  <!-- アイコンとユーザー名 -->
-  <!-- <div class="profile__user col-md-5">
-            <img
-              :src="$storage('icon') + user.icon"
-              class="rounded-circle"
-              width="100"
-              height="100"
-            />
+          <v-col cols="12" sm="6">
+            <v-avatar size="102"><img :src="$storage('icon') + user.icon" /></v-avatar>
             <p class="profile__user--handlename">{{ user.handlename }}</p>
             <p class="profile__user--username">{{ '@' + user.username }}</p>
-          </div>
 
-          <div class="col-md-7">
-            <div class="profile__button"> -->
-  <!-- マイページの場合 -->
-  <!-- <router-link
-                class="btn btn-cloto-primary"
-                :to="{
-                  name: 'profileEdit',
-                  params: { username: $store.getters['auth/user'].username },
-                }"
-                v-if="user.id == $store.getters['auth/user'].id"
-                >編集する</router-link
-              >
-
-                    <v-btn color="yellow darken-1" @click="profileDialog = false">
-        <span class="white--text">CLOSE</span>
-      </v-btn>
-            </div> -->
-  <!-- ボタン類 -->
-  <!-- <div class="profile__sns-container" v-if="sns || user.web">
+            <div class="profile__sns-container" v-if="sns || user.web">
               <a
                 class="profile__sns--twitter"
                 :href="'https://twitter.com/' + sns.twitter"
@@ -102,14 +43,22 @@
                 <i class="fas fa-link fa-2x"></i>
               </a>
             </div>
-          </div>
-        </div>
+          </v-col>
+          <v-col cols="12" sm="6">
+            <v-card-text class="pa-2" min-height="240" color="grey darken-1" dark>
+              {{ user.introduction }}
+            </v-card-text>
+          </v-col>
+        </v-row>
 
-        <div class="profile__introduction" v-if="user.introduction">
-          <p>{{ user.introduction }}</p>
-        </div>
-      </div>
-    </div> -->
+        <v-tabs class="pa-0" fixed-tabs background-color="grey lighten-1" dark>
+          <v-tab> Projects </v-tab>
+          <v-tab> TO Do </v-tab>
+          <v-tab> Record </v-tab>
+        </v-tabs>
+      </v-container>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
@@ -154,5 +103,24 @@ export default {
   width: 500px;
   border: none;
   border-radius: 30px;
+}
+
+.profile__sns--twitter {
+  color: $twitter-color;
+}
+
+.profile__sns--github {
+  color: $github-color;
+}
+
+.profile__sns--qiita {
+  color: $qiita-color;
+  padding-bottom: 0px;
+  margin-bottom: 0px;
+}
+.profile__user--handlename {
+  font-size: 2em;
+  padding-bottom: 0px;
+  margin-bottom: 0px;
 }
 </style>
