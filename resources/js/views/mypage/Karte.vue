5379fcc7eb942e2e36bbeb8afb94dc5548d7e60e
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2020-11-02 10:32:41 +0900

    Closes #31, #32
    DBからカルテを取得する部分の実装

--- resources/js/views/mypage/Karte.vue
+++ resources/js/views/mypage/Karte.vue
@@ -1,105 +1,97 @@
 <template>
+  <v-container pa-0>
+    <!-- ローディングバー -->
+    <v-progress-linear indeterminate color="blue" class="mb-0" v-if="!projects"></v-progress-linear>
 
-<v-row no-gutters>
- <v-col cols="2" class="pa-0 " > 
-    <v-card flat tile class="ma-0"  min-height="700" color="grey darken-1" >
-    <v-list nav　permanent color="grey darken-1">
-        <v-subheader>プロジェクト</v-subheader>
-       <v-list-item-group
-        color="primary"
-        v-model="selectedProject"
-      >
-        <v-list-item
-          v-for="(item, i) in myProjectItems"
-          :key="i"
-            color="grey"
-        >
-          <v-list-item-content>
-            <v-list-item-title v-text="item.text"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
-    {{selectedProject}}
-   </v-card>
-  </v-col>
+    <v-row v-else>
+      <v-col cols="2" class="pa-0">
+        <v-card flat tile class="ma-0" min-height="700" color="grey darken-1">
+          <v-list nav　permanent color="grey darken-1">
+            <v-subheader>プロジェクト</v-subheader>
+            <v-list-item-group color="primary">
+              <v-list-item v-for="project in projects" :key="project.id" color="primary">
+                <v-list-item-content @click="getTasks(project.id)">
+                  <v-list-item-title v-text="project.name"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
+      </v-col>
 
-   <v-col cols="2" class="pa-0 "> 
-    <v-card flat tile class="ma-0"  min-height="700" color="grey lighten-1">
-    <v-list nav　permanent color="grey lighten-1">
-        <v-subheader>タスク</v-subheader>
-       <v-list-item-group
-        color="primary"
-        v-model="selectedTask"
-      >
-        <v-list-item
-          v-for="(item, i) in myTaskItems"
-          :key="i"
-            color="grey"
-        >
-          <v-list-item-content>
-            <v-list-item-title v-text="item.text"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
-    {{selectedProject}}
-   </v-card>
-  </v-col>
+      <v-col cols="2" class="pa-0">
+        <v-card flat tile class="ma-0" min-height="700" color="grey lighten-1">
+          <v-list nav　permanent color="grey lighten-1">
+            <v-subheader>タスク</v-subheader>
+            <v-list-item-group color="primary">
+              <v-list-item v-for="task in tasks" :key="task.id" color="primary">
+                <v-list-item-content @click="getKartes(task.id)">
+                  <v-list-item-title v-text="task.body"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
+      </v-col>
 
-     <v-col cols="8" class="pa-0" > 
-    <v-card flat tile class="ma-0" min-height="700" color="grey lighten-2" >
-    <v-list nav　permanent color="grey lighten-2">
-        <v-subheader>カルテ</v-subheader>
-       <v-list-item-group
-        color="primary"
-
-      >
-        <v-list-item
-          v-for="(item, i) in myKartes"
-          :key="i"
-            color="grey"
-        >
-          <v-list-item-content>
-            <v-list-item-title v-text="item.text"></v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list-item-group>
-    </v-list>
-   </v-card>
-  </v-col>
- </v-row> 
-
+      <v-col cols="8" class="pa-0">
+        <v-card flat tile class="ma-0" min-height="700" color="grey lighten-2">
+          <v-list nav　permanent color="grey lighten-2">
+            <v-subheader>カルテ</v-subheader>
+            <v-list-item-group color="primary">
+              <v-list-item v-for="karte in kartes" :key="karte.id" color="grey">
+                <v-list-item-content>
+                  <v-list-item-title v-text="karte.body"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      selectedMyPage:0,
-      selectedProject:null,
-      selectedTask:null,
-
-      myProjectItems:[
-        {text:'英語'},
-        {text:'数学'},
-        {text:'国語'},
-      ],
-
-      myTaskItems:[
-        {text:'英単語10分'},
-        {text:'長文問題１つ'},
-        {text:'シャドーイング'}
-      ],
-
-      myKartes:[
-        {text:'aaaaaaaaaaa'}
-      ],
+      projects: '',
+      tasks: '',
+      kartes: '',
     };
-  }
-}
+  },
+  methods: {
+    /**
+     * プロジェクトの取得
+     */
+    getProjects: async function () {
+      var response = await this.$http.get(this.$endpoint('projectIndex'));
+      this.projects = response.data;
+    },
+
+    /**
+     * タスクの取得
+     *
+     * @param Number  projectId 取得するタスクのプロジェクトID
+     */
+    getTasks: async function (projectId) {
+      var response = await this.$http.get(this.$endpoint('taskIndex', [projectId]));
+      this.tasks = response.data;
+    },
+
+    /**
+     * カルテの取得
+     *
+     * @param Number  taskId 取得するカルテのタスクID
+     */
+    getKartes: async function (taskId) {
+      var response = await this.$http.get(this.$endpoint('karteIndexFromTask', [taskId]));
+      this.kartes = response.data;
+    },
+  },
+  mounted() {
+    this.getProjects();
+  },
+};
 </script>
-
-<style>
-
-</style>
