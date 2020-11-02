bfb5c134f128837baafb69aaa4b35436baf61c32
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2020-11-02 11:46:39 +0900

    #17 ローディングの実装

--- resources/js/views/mypage/Karte.vue
+++ resources/js/views/mypage/Karte.vue
@@ -1,15 +1,18 @@
 <template>
-  <v-container pa-0>
-    <!-- ローディングバー -->
-    <v-progress-linear indeterminate color="blue" class="mb-0" v-if="!projects"></v-progress-linear>
+  <v-container py-0>
+    <v-row>
+      <v-col class="pa-0">
+        <v-card tile min-height="700">
+          <v-skeleton-loader
+            type="card-heading, list-item@10"
+            class="pt-1 px-4"
+            v-if="projects.loading"
+          ></v-skeleton-loader>
 
-    <v-row v-else>
-      <v-col cols="2" class="pa-0">
-        <v-card flat tile class="ma-0" min-height="700" color="grey darken-1">
-          <v-list nav　permanent color="grey darken-1">
+          <v-list nav permanent v-else>
             <v-subheader>プロジェクト</v-subheader>
             <v-list-item-group color="primary">
-              <v-list-item v-for="project in projects" :key="project.id" color="primary">
+              <v-list-item v-for="project in projects.data" :key="project.id" color="primary">
                 <v-list-item-content @click="getTasks(project.id)">
                   <v-list-item-title v-text="project.name"></v-list-item-title>
                 </v-list-item-content>
@@ -19,12 +22,18 @@
         </v-card>
       </v-col>
 
-      <v-col cols="2" class="pa-0">
-        <v-card flat tile class="ma-0" min-height="700" color="grey lighten-1">
-          <v-list nav　permanent color="grey lighten-1">
+      <v-col class="pa-0">
+        <v-card tile min-height="700">
+          <v-skeleton-loader
+            type="card-heading, list-item@10"
+            class="pt-1 px-4"
+            v-if="tasks.loading"
+          ></v-skeleton-loader>
+
+          <v-list nav permanent v-else>
             <v-subheader>タスク</v-subheader>
             <v-list-item-group color="primary">
-              <v-list-item v-for="task in tasks" :key="task.id" color="primary">
+              <v-list-item v-for="task in tasks.data" :key="task.id" color="primary">
                 <v-list-item-content @click="getKartes(task.id)">
                   <v-list-item-title v-text="task.body"></v-list-item-title>
                 </v-list-item-content>
@@ -34,12 +43,18 @@
         </v-card>
       </v-col>
 
-      <v-col cols="8" class="pa-0">
-        <v-card flat tile class="ma-0" min-height="700" color="grey lighten-2">
-          <v-list nav　permanent color="grey lighten-2">
+      <v-col class="pa-0">
+        <v-card tile min-height="700">
+          <v-skeleton-loader
+            type="card-heading, list-item@10"
+            class="pt-1 px-4"
+            v-if="kartes.loading"
+          ></v-skeleton-loader>
+
+          <v-list nav permanent v-else>
             <v-subheader>カルテ</v-subheader>
             <v-list-item-group color="primary">
-              <v-list-item v-for="karte in kartes" :key="karte.id" color="grey">
+              <v-list-item v-for="karte in kartes.data" :key="karte.id" color="grey">
                 <v-list-item-content>
                   <v-list-item-title v-text="karte.body"></v-list-item-title>
                 </v-list-item-content>
@@ -56,9 +71,18 @@
 export default {
   data() {
     return {
-      projects: '',
-      tasks: '',
-      kartes: '',
+      projects: {
+        data: '',
+        loading: false,
+      },
+      tasks: {
+        data: '',
+        loading: false,
+      },
+      kartes: {
+        data: '',
+        loading: false,
+      },
     };
   },
   methods: {
@@ -66,8 +90,12 @@ export default {
      * プロジェクトの取得
      */
     getProjects: async function () {
+      this.projects.loading = true;
+
       var response = await this.$http.get(this.$endpoint('projectIndex'));
-      this.projects = response.data;
+      this.projects.data = response.data;
+
+      this.projects.loading = false;
     },
 
     /**
@@ -76,8 +104,13 @@ export default {
      * @param Number  projectId 取得するタスクのプロジェクトID
      */
     getTasks: async function (projectId) {
+      this.tasks.loading = true;
+      this.kartes.data = '';
+
       var response = await this.$http.get(this.$endpoint('taskIndex', [projectId]));
-      this.tasks = response.data;
+      this.tasks.data = response.data;
+
+      this.tasks.loading = false;
     },
 
     /**
@@ -86,8 +119,12 @@ export default {
      * @param Number  taskId 取得するカルテのタスクID
      */
     getKartes: async function (taskId) {
+      this.kartes.loading = true;
+
       var response = await this.$http.get(this.$endpoint('karteIndexFromTask', [taskId]));
-      this.kartes = response.data;
+      this.kartes.data = response.data;
+
+      this.kartes.loading = false;
     },
   },
   mounted() {
