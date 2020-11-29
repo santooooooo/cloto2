3ddcec9dd46555517723174c184d9b0bae72ee35
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2020-11-29 20:28:38 +0900

    バックアップの作成

---
+++ resources/js/backups/components/mypage/Drawer.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-card class="mx-auto" width="250" flat tile>
+    <v-navigation-drawer permanent>
+      <v-list>
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="text-h5">マイページ</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+
+      <v-list nav dense>
+        <v-list-item-group color="#f6bf00">
+          <v-list-item class="mb-4" :to="{ name: 'mypage' }">
+            <v-list-item-content>
+              <v-list-item-title class="text-body-1 font-weight-bold"
+                >プロフィール</v-list-item-title
+              >
+            </v-list-item-content>
+          </v-list-item>
+
+          <v-list-item class="mb-4" :to="{ name: 'karte' }">
+            <v-list-item-content>
+              <v-list-item-title class="text-body-1 font-weight-bold">カルテ</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </v-card>
+</template>
+
+<style lang="scss" scoped>
+a:hover {
+  text-decoration: none;
+}
+</style>
---
+++ resources/js/backups/components/mypage/ProfileEditDialog.vue
@@ -0,0 +1,245 @@
+<template>
+  <v-dialog v-model="dialog" width="650" persistent>
+    <v-form ref="profileUpdateForm" v-model="profileUpdateForm.validation.valid" lazy-validation>
+      <v-card class="headline grey darken-2 text-center px-2">
+        <v-container>
+          <!-- アイコン -->
+          <InputImage
+            :no-change-crop-ratio="true"
+            ratio-x="1"
+            ratio-y="1"
+            @input="profileUpdateForm.icon = $event"
+          />
+    
+          <v-row>
+            <v-col>
+              <!-- ユーザー名 -->
+              <v-card-text class="pa-1 white--text">ユーザー名</v-card-text>
+              <v-text-field
+                v-model="profileUpdateForm.username"
+                :rules="profileUpdateForm.validation.usernameRules"
+                label="ユーザー名"
+                solo
+                rounded
+                class="pa-2"
+              ></v-text-field>
+            </v-col>
+
+            <v-col>
+              <!-- アカウント名 -->
+              <v-card-text class="pa-1 white--text">アカウント名</v-card-text>
+              <v-text-field
+                v-model="profileUpdateForm.handlename"
+                :rules="profileUpdateForm.validation.accountRules"
+                label="アカウント名"
+                solo
+                rounded
+                class="pa-2"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+          <!-- メールアドレス -->
+          <v-card-text class="pa-1 white--text">メールアドレス</v-card-text>
+          <v-text-field
+            v-model="profileUpdateForm.email"
+            label="メールアドレス"
+            solo
+            rounded
+            class="pa-2"
+          ></v-text-field>
+
+          <!-- Twitter -->
+          <v-row>
+            <v-col>
+              <v-card-text class="pa-1 white--text">Twitter</v-card-text>
+              <v-text-field
+                v-model="profileUpdateForm.sns.twitter"
+                placeholder="@以降を入力 例：CLOTO_JP"
+                solo
+                rounded
+                class="pa-2"
+              >
+              </v-text-field>
+            </v-col>
+
+            <!-- GitHub -->
+            <v-col>
+              <v-card-text class="pa-1 white--text">GitHub</v-card-text>
+              <v-text-field
+                v-model="profileUpdateForm.sns.github"
+                placeholder="GitHub 例：CLOTO_JP"
+                solo
+                rounded
+                class="pa-2"
+              >
+              </v-text-field>
+            </v-col>
+          </v-row>
+
+          <v-row>
+            <!-- Qiita -->
+            <v-col>
+              <v-card-text class="pa-1 white--text">Qiita</v-card-text>
+              <v-text-field
+                v-model="profileUpdateForm.sns.qiita"
+                placeholder="Qiita 例：CLOTO_JP"
+                solo
+                rounded
+                class="pa-2"
+              >
+              </v-text-field>
+            </v-col>
+
+            <!-- ホームページ -->
+            <v-col>
+              <v-card-text class="pa-1 white--text">Webサイト</v-card-text>
+              <v-text-field
+                v-model="profileUpdateForm.web"
+                placeholder="Webサイト 例：https://cloto.jp"
+                solo
+                rounded
+                class="pa-2"
+              >
+              </v-text-field>
+            </v-col>
+          </v-row>
+
+          <!-- 自己紹介 -->
+          <v-card-text class="pa-1 white--text">自己紹介</v-card-text>
+          <v-textarea
+            solo
+            name="introduction"
+            rounded
+            rows="3"
+            class="pa-1"
+            v-model="profileUpdateForm.introduction"
+          ></v-textarea>
+
+          <!-- ボタン -->
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              @click="$emit('close', false)"
+              depressed
+              class="mt-3 mr-8 white--text"
+              color="error"
+            >
+              閉じる
+            </v-btn>
+
+            <v-btn
+              @click="submit()"
+              :loading="profileUpdateForm.loading"
+              :disabled="!profileUpdateForm.validation.valid"
+              depressed
+              class="mt-3 ml-8 white--text"
+              color="#f6bf00"
+            >
+              更新
+            </v-btn>
+            <v-spacer></v-spacer>
+          </v-card-actions>
+        </v-container>
+      </v-card>
+    </v-form>
+  </v-dialog>
+</template>
+
+<script>
+import { OK } from '@/consts/status';
+
+export default {
+  head: {
+    title() {
+      return {
+        inner: this.$route.params.username + ' - ' + '編集',
+      };
+    },
+  },
+  data() {
+    return {
+      dialog: true,
+      profileUpdateForm: {
+        username: '',
+        email: '',
+        handlename: '',
+        icon: '',
+        web: '',
+        introduction: '',
+        loading: false,
+        sns:{
+        twitter: '',
+        github: '',
+        qiita: '',
+        },
+        validation: {
+          valid: false,
+          usernameRules: [(v) => !!v || 'ユーザネームは必須項目です。'],
+          accountRules: [(v) => !!v || 'アカウント名は必須項目です。'],
+        },
+      },
+    };
+  },
+
+  computed: {
+    authUser() {
+      return this.$store.getters['auth/user'];
+    },
+  },
+
+  methods: {
+    submit: async function () {
+      if (this.$refs.profileUpdateForm.validate()) {
+        this.profileUpdateForm.loading = true;
+
+        var input = new FormData();
+        input.append('username', this.profileUpdateForm.username);
+        input.append('email', this.profileUpdateForm.email);
+        input.append('handlename', this.profileUpdateForm.handlename);
+        input.append('icon', this.profileUpdateForm.icon);
+        input.append(
+          'twitter',
+          this.profileUpdateForm.sns.twitter ? this.profileUpdateForm.sns.twitter : ''
+        );
+        input.append('github', this.profileUpdateForm.sns.github ? this.profileUpdateForm.sns.github : '');
+        input.append('qiita', this.profileUpdateForm.sns.qiita ? this.profileUpdateForm.sns.qiita : '');
+        input.append('web', this.profileUpdateForm.web ? this.profileUpdateForm.web : '');
+        input.append(
+          'introduction',
+          this.profileUpdateForm.introduction ? this.profileUpdateForm.introduction : ''
+        );
+
+        // ユーザーデータ保存処理
+        var response = await this.$http.post(this.$endpoint('profileUpdate'), input);
+
+        if (response.status === OK) {
+          this.$store.dispatch('alert/show', {
+            type: 'success',
+            message: 'ユーザーデータが更新されました。',
+          });
+
+          // ユーザーデータの同期
+          await this.$store.dispatch('auth/syncAuthUser');
+
+          this.dialog = false;
+          this.$emit('close', false);
+        } else {
+          this.$store.dispatch('alert/show', {
+            type: 'error',
+            message: 'エラーが発生しました。',
+          });
+
+          this.profileUpdateForm.loading = false;
+        }
+      }
+    },
+  },
+
+  mounted() {
+    Object.assign(this.profileUpdateForm, this.authUser);
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
