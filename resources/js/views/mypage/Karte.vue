6853201aa96d3435bddcb822cec33b6d2c6b996c
shutoushu <shutoushu@gmail.com>
2020-12-19 19:05:12 +0900

    アイテム数の指定以外完成
    Co-Authored-By: SayukiShingai <67946568+SayukiShingai@users.noreply.github.com>

--- resources/js/views/mypage/Karte.vue
+++ resources/js/views/mypage/Karte.vue
@@ -4,10 +4,21 @@
     <v-overlay v-if="!kartes.data">
       <v-progress-circular indeterminate size="64" v-if="!kartes.data"></v-progress-circular>
     </v-overlay>
-    <v-card max-width="1080" tile class="ml-10">
-      <!-- <v-row v-on:mouseover"open" width="225px">aaaaaaaaaaaaaaaaaaaaaa </v-row> -->
 
-      <v-simple-table>
+    <!-- <v-menu open-on-hover top offset-y>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn color="primary" dark v-bind="attrs" v-on="on"> Dropdown </v-btn>
+      </template>
+
+      <v-list>
+        <v-list-item v-for="(item, index) in kartes.data" :key="index">
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu> -->
+
+    <v-card max-width="1080" tile class="ml-10">
+      <v-simple-table headers-length:5>
         <template v-slot:default>
           <thead>
             <tr>
@@ -25,9 +36,10 @@
               <td v-if="karte.image != null">
                 <p class="karteContent">
                   <v-img
+                    style="cursor: pointer"
+                    @click="openKarte(karte)"
                     max-width="120"
                     class="mt-2"
-                    v-on:mouseover="openKarte(karte.body)"
                     contain
                     :src="$storage('karte') + karte.image"
                   />
@@ -37,181 +49,113 @@
                 <p class="karteContent">None</p>
               </td>
 
-              <td v-if="karte.body != null">
-                <p class="karteContent">{{ karte.body }}</p>
-              </td>
-              <td v-else>
-                <p class="karteContent">None</p>
-              </td>
+              <v-tooltip max-width="300" top>
+                <template v-slot:activator="{ on, attrs }">
+                  <td v-bind="attrs" v-on="on" v-if="karte.body != null">
+                    <p class="karteContent">{{ karte.body }}</p>
+                  </td>
+
+                  <td v-else>
+                    <p class="karteContent">None</p>
+                  </td>
+                </template>
+                <span>{{ karte.body }}</span>
+              </v-tooltip>
+
               <td>
                 <p class="karteContent">00:00</p>
               </td>
-              <td v-if="karte.achieve != null">
-                <p class="karteContent">{{ karte.achieve }}</p>
+              <v-tooltip max-width="300" top>
+                <template v-slot:activator="{ on, attrs }">
+                  <td v-bind="attrs" v-on="on" v-if="karte.archive != null">
+                    <p class="karteContent">{{ karte.archive }}</p>
+                  </td>
+
+                  <td v-else>
+                    <p class="karteContent">None</p>
+                  </td>
+                </template>
+                <span>{{ karte.archive }}</span>
+              </v-tooltip>
+              <v-tooltip max-width="300" top>
+                <template v-slot:activator="{ on, attrs }">
+                  <td v-bind="attrs" v-on="on" v-if="karte.trouble != null">
+                    <p class="karteContent">{{ karte.trouble }}</p>
+                  </td>
+
+                  <td v-else>
+                    <p class="karteContent">None</p>
+                  </td>
+                </template>
+                <span>{{ karte.trouble }}</span>
+              </v-tooltip>
+              <v-tooltip max-width="300" top>
+                <template v-slot:activator="{ on, attrs }">
+                  <td v-bind="attrs" v-on="on" v-if="karte.reference != null">
+                    <p class="karteContent">{{ karte.reference }}</p>
+                  </td>
+
+                  <td v-else>
+                    <p class="karteContent">None</p>
+                  </td>
+                </template>
+                <span>{{ karte.reference }}</span>
+              </v-tooltip>
+
+              <v-tooltip max-width="300" top>
+                <template v-slot:activator="{ on, attrs }">
+                  <td v-bind="attrs" v-on="on" v-if="karte.technologies.length != 0">
+                    <p class="karteContent">技術タグ{{ karte.technologies.length }}個</p>
+                  </td>
+
+                  <td v-else>
+                    <p class="karteContent">None</p>
+                  </td>
+                </template>
+
+                <v-chip
+                  class="ma-2"
+                  filter
+                  v-for="technology in karte.technologies"
+                  :key="technology.id"
+                  :value="technology.id"
+                >
+                  {{ technology.name }}
+                </v-chip>
+              </v-tooltip>
+
+              <!-- <td v-if="karte.technologies.length != 0">
+                <p class="karteContent">技術タグ{{ karte.technologies.length }}個</p>
               </td>
               <td v-else>
                 <p class="karteContent">None</p>
-              </td>
-              <td v-if="karte.trouble != null">
-                <p class="karteContent">{{ karte.trouble }}</p>
-              </td>
-              <td v-else>
-                <p class="karteContent">None</p>
-              </td>
-              <td v-if="karte.reference != null">
-                <p class="karteContent">{{ karte.reference }}</p>
-              </td>
-              <td v-else>
-                <p class="karteContent">None</p>
-              </td>
-              <td v-if="karte.technologies.length != 0">
-                <p class="karteContent">{{ karte.technologies }}</p>
-              </td>
-              <td v-else>
-                <p class="karteContent">None</p>
-              </td>
+              </td> -->
             </tr>
           </tbody>
         </template>
       </v-simple-table>
 
-      <!-- <v-list>
-        <v-subheader>
-          <v-row justify="center">
-            <v-col> 画像 </v-col>
-            <v-col> 内容 </v-col>
-            <v-col> 投稿時間 </v-col>
-            <v-col> 達成 </v-col>
-            <v-col> できなかったこと </v-col>
-            <v-col>文献</v-col>
-            <v-col>技術タグ</v-col>
-          </v-row>
-        </v-subheader>
-        <v-list-item-group v-model="selectedItem">
-          <v-list-item v-for="karte in kartes.data" :key="karte.id">
-            <v-list-item-content @click="openKarte(karte)">
-              <v-row>
-                <v-col
-                  ><v-img
-                    max-width="120"
-                    v-on:mouseover="openKarte(karte.body)"
-                    contain
-                    :src="$storage('karte') + karte.image"
-                  />
-                </v-col>
-                <v-col class="col-2 text-truncate pa-0"> {{ karte.body }} </v-col>
-                <v-col class="text-truncate pa-0"> 活動時間 </v-col>
-                <v-col class="text-truncate pa-0"> {{ karte.achieve }} </v-col>
-                <v-col class="text-truncate pa-0"> {{ karte.trouble }} </v-col>
-                <v-col class="text-truncate pa-0"> {{ karte.reference }} </v-col>
-                <v-col class="text-truncate pa-0"> 技術タグ </v-col>
-              </v-row>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list> -->
-
-      <v-dialog v-model="dialog" width="300">
+      <v-dialog v-model="dialog" width="700">
         <v-card>
-          <v-card-text>
-            {{ this.chosenKarte }}
-          </v-card-text>
+          <v-img contain :src="$storage('karte') + chosenKarte.image" />
         </v-card>
       </v-dialog>
-
-      <!-- <v-dialog v-model="dialog" width="800">
-        <v-card color="grey darken-1">
-          <v-container>
-            {{ kartes.data }}
-            <v-row justify="center">
-              <v-col>
-                <v-card-text class="pa-2 white--text title font-weight-bold">
-                  活動内容
-                </v-card-text>
-                <v-card rounded>
-                  <div>{{ chosenKarte.body }}</div></v-card
-                >
-
-                <v-card-text class="pa-2 white--text title font-weight-bold">
-                  参考文献
-                </v-card-text>
-                <v-card solo rounded v-if="chosenKarte.reference != null">{{
-                  chosenKarte.reference
-                }}</v-card>
-                <v-card solo rounded v-else>なし</v-card>
-              </v-col>
-
-              <v-col>
-                <v-card-text class="pa-0 white--text title font-weight-bold"> 画像 </v-card-text>
-
-                <v-card v-if="chosenKarte.image != null" height="200" class="text-center pt-6">
-                  <v-avatar class="profile" color="grey" size="150">
-                    <img :src="$storage('karte') + chosenKarte.image" />
-                  </v-avatar>
-                </v-card>
-
-                <v-card v-else> なし </v-card>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col>
-                <v-card-text class="pa-2 white--text title font-weight-bold">
-                  技術タグ
-                </v-card-text>
-                <v-card
-                  solo
-                  rounded
-                  v-if="chosenKarte.technologies && chosenKarte.technologies.length !== 0"
-                  ><span v-for="technology in chosenKarte.technologies" :key="technology.id"
-                    >{{ technology.name }}　</span
-                  ></v-card
-                >
-
-                <v-card v-else>なし</v-card>
-              </v-col>
-
-              <v-col>
-                <v-card-text class="pa-2 white--text title font-weight-bold">
-                  活動時間
-                </v-card-text>
-                <v-card solo rounded>a</v-card>
-              </v-col>
-            </v-row>
-
-            <v-row>
-              <v-col>
-                <v-card-text class="pa-2 white--text title font-weight-bold">
-                  達成したこと
-                </v-card-text>
-                <v-card rounded v-if="chosenKarte.achieve != null">
-                  {{ chosenKarte.achieve }}</v-card
-                >
-
-                <v-card rounded v-else> 特になし</v-card>
-              </v-col>
-
-              <v-col>
-                <v-card-text class="pa-2 white--text title font-weight-bold">
-                  できなかったこと
-                </v-card-text>
-                <v-card rounded v-if="chosenKarte.trouble != null">{{
-                  chosenKarte.trouble
-                }}</v-card>
-
-                <v-card rounded v-else>特になし</v-card>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-card>
-      </v-dialog> -->
     </v-card>
   </v-container>
 </template>
 
 <script>
 export default {
+  head: {
+    title() {
+      return {
+        inner: 'カルテ',
+      };
+    },
+  },
   data() {
     return {
+      showMenu: false,
       chosenKarte: '',
       projects: {
         data: '',
@@ -280,7 +224,6 @@ export default {
     openKarte: function (content) {
       this.dialog = true;
       this.chosenKarte = content;
-      console.log(content);
     },
   },
 
@@ -293,10 +236,6 @@ export default {
 
 <style lang="scss" scoped>
 @import '~/_variables';
-
-element.style {
-  width: 0px;
-}
 
 .karteContent {
   overflow: hidden;
