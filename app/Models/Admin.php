241c4e358f49ca2cd571983ce230dd144c194991
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2020-12-22 17:48:43 +0900

    #46 adminsテーブルの作成

---
+++ app/Models/Admin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Admin extends Authenticatable
+{
+    use Notifiable;
+
+    protected $primaryKey = 'id';
+    protected $dates = ['created_at', 'updated_at'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'username', 'email', 'password', 'handlename'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime'
+    ];
+}
---
+++ database/migrations/2020_12_22_173650_create_admins_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use App\Database\Blueprint;
+use App\Facades\Schema;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('username')->unique();               // ユーザー名
+            $table->string('email')->unique();                  // メール
+            $table->dateTime('email_verified_at')->nullable();  // メール認証日時
+            $table->string('password');                         // パスワード
+            $table->string('handlename')->nullable();           // 表示名
+            $table->rememberToken();
+            $table->dateTimes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
