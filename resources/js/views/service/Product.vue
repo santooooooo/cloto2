2957370d412704e92096c210dac5b1a8b166fd95
shutoushu <shutoushu@gmail.com>
2020-12-23 18:25:17 +0900

    subtitle をkeyに紐づけられず

--- resources/js/views/service/Product.vue
+++ resources/js/views/service/Product.vue
@@ -1,15 +1,12 @@
 <template>
   <div fluid class="index">
-    <v-container class="upper">
-      <v-container>
-        <Carousel></Carousel>
-        <v-row justify="center" class="mt-12">
-          <router-link :to="{ name: 'preRegister' }">
-            <v-btn x-large color="primary" class="font-weight-bold">新規登録</v-btn>
-          </router-link>
-        </v-row>
-      </v-container>
-    </v-container>
+    <Carousel></Carousel>
+    <v-row justify="center" class="mt-12">
+      <router-link :to="{ name: 'preRegister' }">
+        <v-btn x-large color="primary" class="font-weight-bold">新規登録</v-btn>
+      </router-link>
+    </v-row>
+
     <v-container class="content">
       <v-row justify="center">
         <v-row class="mt-12">
