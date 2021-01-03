e5244d66b122efcad0dfb59c621bb7a594555628
shutoushu <shutoushu@gmail.com>
2021-01-03 12:29:54 +0900

    #41 carousel上にボタンの配置 ボタンリンクの作成
    Co-Authored-By: y0sh1z0 <58413298+y0sh1z0@users.noreply.github.com>
    Co-Authored-By: a2h1r0 <53073064+a2h1r0@users.noreply.github.com>

--- resources/js/components/service/Carousel.vue
+++ resources/js/components/service/Carousel.vue
@@ -9,9 +9,12 @@
     >
     <v-carousel hide-delimiters>
       <!-- height="autoを挙動がおかしくなる" -->
-      <v-carousel-item v-for="(image, i) in images" :key="i" :src="image.src">
+      <v-carousel-item v-for="(page, i) in pages" :key="i" :src="page.image">
         <v-row style="height: 87%" align="center" justify="center">
-          <div class="display-3">{{ image.title }}</div>
+          <div style="width: 100%" class="display-3 text-center">
+            <p class="mb-0">{{ page.title }}</p>
+            <v-btn :to="{ name: page.to }">{{ page.btnText }}</v-btn>
+          </div>
         </v-row>
         <v-row
           class="grey darken-3 pl-16 pr-16 pt-1 pb-1"
@@ -19,13 +22,8 @@
           justify="center"
           style="height: 13%"
         >
-          <div>{{ image.subtitle }}</div>
+          <div>{{ page.body }}</div>
         </v-row>
-        <!-- <v-list class="text-center" color="grey darken-3"
-          ><v-list-item-content>
-            <v-list-item-title class="text-white">{{ image.subtitle }}</v-list-item-title>
-          </v-list-item-content></v-list
-        > -->
       </v-carousel-item>
     </v-carousel>
   </div>
@@ -34,44 +32,56 @@
 export default {
   data() {
     return {
-      imageIndex: 0,
-      images: [
+      pages: [
         {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          image: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
           title: 'オンライン学習施設',
-          subtitle: 'CLOTOとは学習目的、学習内容ごとに分けられた部屋が複数ある自習施設です。',
+          body: 'CLOTOとは学習目的学習内容ごとに分けられた部屋が複数ある自習施設です。',
+          btnText: '登録',
+          to: 'register',
         },
         {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          image: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
           title: '自習室',
-          subtitle: '自習室では、同じ時間に、同じ学習に取り組んでいる仲間が表示されています',
+          body: '自習室では、同じ時間に、同じ学習に取り組んでいる仲間が表示されています',
+          btnText: 'ログイン',
+          to: 'login',
         },
         {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+          image: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
           title: '休憩室',
-          subtitle: '学校の時間割のように、CLOTO全体にも定期的に自習時間の間に休憩時間が訪れます。',
+          body: '学校の時間割のように、CLOTO全体にも定期的に自習時間の間に休憩時間が訪れます。',
+          btnText: 'ログイン',
+          to: 'login',
         },
         {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+          image: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
           title: 'メンタリングルーム',
-          subtitle:
+          body:
             'CLOTOでは段階的に2種類のメンタリング機能を用意し、みなさんの技術的課題のサポートをします。',
+          btnText: 'ログイン',
+          to: 'login',
         },
         {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          image: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
           title: 'カルテ・ポートフォリオ',
-          subtitle: 'CLOTOとは学習目的、学習内容ごとに分けられた部屋が複数ある自習施設です。',
+          body: 'CLOTOとは学習目的、学習内容ごとに分けられた部屋が複数ある自習施設です。',
+          btnText: 'ログイン',
+          to: 'login',
         },
         {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          image: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
           title: 'イベントルーム',
-          subtitle: 'CLOTOとは学習目的、学習内容ごとに分けられた部屋が複数ある自習施設です。',
+          body: 'CLOTOとは学習目的、学習内容ごとに分けられた部屋が複数ある自習施設です。',
+          btnText: 'ログイン',
+          to: 'login',
         },
         {
-          src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          image: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
           title: 'イベントルーム',
-          subtitle:
-            'CLOTOとは学習目的、学習内容ごとに分けられた部屋が複数ある自習施設です。aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
+          body: 'CLOTOとは学習目的、学習内容ごとに分けられた部屋が複数ある自習施設です。',
+          btnText: 'ログイン',
+          to: 'login',
         },
       ],
     };
--- resources/js/views/service/Product.vue
+++ resources/js/views/service/Product.vue
@@ -1,15 +1,18 @@
 <template>
   <div fluid class="index">
     <Carousel></Carousel>
-    <v-row justify="center" class="mt-12">
+    <!-- <v-row justify="center" class="mt-12">
       <router-link :to="{ name: 'preRegister' }">
         <v-btn x-large color="primary" class="font-weight-bold">新規登録</v-btn>
       </router-link>
-    </v-row>
+    </v-row> -->
 
-    <v-container class="content">
+    <h3 class="text-center mt-16">製品紹介</h3>
+
+    <v-container class="content mt-8">
+      <h2 class="text-center mt-2">ベータ版</h2>
       <v-row justify="center">
-        <v-row class="mt-12">
+        <v-row class="productInfo mt-6">
           <v-col>
             <v-img eager width="500" :src="$storage('system') + 'P3.svg'"></v-img>
           </v-col>
@@ -22,7 +25,7 @@
           </v-col>
         </v-row>
 
-        <v-row class="mt-12">
+        <v-row class="productInfo mt-12">
           <v-col>
             <v-img eager width="500" :src="$storage('system') + 'P3.svg'"></v-img>
           </v-col>
@@ -34,7 +37,7 @@
             </p>
           </v-col>
         </v-row>
-        <v-row class="mt-12">
+        <v-row class="productInfo mt-12">
           <v-col>
             <v-img eager width="500" :src="$storage('system') + 'P3.svg'"></v-img>
           </v-col>
@@ -45,18 +48,6 @@
             </p>
           </v-col>
         </v-row>
-
-        <v-card justify="center" class="text-center mt-16">
-          <v-row no-gutters>
-            <v-card-title class="mx-auto">料金体系</v-card-title>
-          </v-row>
-          <v-data-table
-            :headers="headers"
-            :items="desserts"
-            :items-per-page="5"
-            class="elevation-1"
-          ></v-data-table>
-        </v-card>
       </v-row>
     </v-container>
   </div>
@@ -73,101 +64,6 @@ export default {
     return {
       model: 0,
       colors: ['primary', 'secondary', 'yellow darken-2', 'red', 'orange'],
-      headers: [
-        {
-          text: 'Dessert (100g serving)',
-          align: 'start',
-          sortable: false,
-          value: 'name',
-        },
-        { text: 'Calories', value: 'calories' },
-        { text: 'Fat (g)', value: 'fat' },
-        { text: 'Carbs (g)', value: 'carbs' },
-        { text: 'Protein (g)', value: 'protein' },
-        { text: 'Iron (%)', value: 'iron' },
-      ],
-      desserts: [
-        {
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: '1%',
-        },
-        {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-          iron: '1%',
-        },
-        {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-          iron: '7%',
-        },
-        {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-          iron: '8%',
-        },
-        {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-          iron: '16%',
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-          iron: '0%',
-        },
-        {
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-          iron: '2%',
-        },
-        {
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-          iron: '45%',
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-          iron: '22%',
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-          iron: '6%',
-        },
-      ],
     };
   },
 };
@@ -178,13 +74,12 @@ export default {
 
 .index {
   background-color: $light-gray;
-  .upper {
-    max-width: 100%;
-    background-color: $primary;
-    color: $white;
-    position: relative;
-  }
   .content {
+    background: white;
+    .productInfo {
+      background-color: yellow;
+      width: 70%;
+    }
   }
 }
 </style>
