e6c33cce67e17ba19d929f1a247e90f7e49b1d3c
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-01-15 22:26:07 +0900

    バックアップスケジュールの更新

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -33,13 +33,16 @@ class Kernel extends ConsoleKernel
         // 3:00にシステムを停止
         $schedule->command('system:down')->dailyAt('3:00');
         $schedule->command('down')->dailyAt('3:00');
-        // 3:10に全員退席処理
-        $schedule->command('refresh:seats')->dailyAt('3:10')->evenInMaintenanceMode();
 
-        //*** cronでバックアップなどを実行 ***//
+        //*** cron ***//
+        // 3:10にSSL証明書を更新（1回目）
+        // 3:20にSSL証明書を更新（2回目）
+        // 3:30にバックアップ開始
 
-        // 4:45にシステムを復旧
-        $schedule->command('up')->dailyAt('4:45')->evenInMaintenanceMode();
+        // 4:40に全員退席処理
+        $schedule->command('refresh:seats')->dailyAt('4:40')->evenInMaintenanceMode();
+        // 4:50にシステムを復旧
+        $schedule->command('up')->dailyAt('4:50')->evenInMaintenanceMode();
 
 
         /**
