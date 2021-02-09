03b3d697de56f17dab1f3b2bce6aaff76d16eb25
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-02-09 13:40:24 +0900

    #45 Slack通知の修正

--- resources/js/plugins/slack.js
+++ resources/js/plugins/slack.js
@@ -1,24 +1,17 @@
-const axios = require('axios');
+export function post(username, icon, message) {
+  // 本番サーバのみ通知
+  if (window.location.hostname === 'cloto.jp') {
+    var data = {
+      channel: process.env.MIX_SLACK_CHANNEL,
+      username: username,
+      icon_emoji: icon,
+      text: message,
+    };
 
-export async function post(username, icon, message) {
-  var data = `payload={
-    'channel': '${process.env.MIX_SLACK_CHANNEL}',
-    'username':'${username}',
-    'icon_emoji':'${icon}',
-    'text': '${message}'
-    }`;
-
-  var param = {
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    },
-  };
-
-  try {
-    if (window.location.hostname === 'cloto.jp') {
-      await axios.post(process.env.MIX_SLACK_URL, data, param);
-    }
-  } catch (error) {
-    // 通知に失敗しても続行（Slackの障害時など）
+    // AxiosではCORSエラーが発生する
+    const xml = new XMLHttpRequest();
+    xml.open('POST', process.env.MIX_SLACK_URL);
+    xml.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded;charset=UTF-8');
+    xml.send(JSON.stringify(data));
   }
 }
