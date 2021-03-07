dbaf08b20c7c5f51e9af70b64abd25f52e865537
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-03-07 17:57:29 +0900

    #57 デザイン調整

--- resources/js/views/timeline/Timeline.vue
+++ resources/js/views/timeline/Timeline.vue
@@ -1,275 +1,148 @@
 <template>
-	  <v-container>
-		  <v-card>
-			  <v-toolbar color="yellow">
-				  <v-toolbar-title>タイムライン</v-toolbar-title>
-				  <v-spacer></v-spacer>
-			  </v-toolbar>
-			  
-			  <v-row>
-				  <v-spacer></v-spacer>
-				  <v-col v-for="karte in kartes" :key="kartes.id" cols="12" sm="6" md="4" @click="showKarte = karte">
-					  <v-card :ripple="true" class="btn text-left" :loading="loading">
-						  <v-card-actions class="white justify-center d-block">
-							  <v-img
-								  max-width="300"
-								  class="mx-auto my-2 rounded-xl"
-								  contain
-								  :src="$karte(karte, authUser.username) + karte.image"
-								  v-if="karte.image"
-							  ></v-img>							  
+  <v-container>
+    <h1 class="text-h4">タイムライン</h1>
 
-					      <v-row align="center">
-						      <v-col class="d-flex" cols="4" sm="2" md="10">
-							      <!-- ユーザーアイコン -->
-									  <v-avatar>
-										  <v-img
-											  :src="$storage('icon') + authUser.icon"
-											  contain
-											  max-width="50">
-										  </v-img>
-									  </v-avatar>
-							      <!-- ユーザー名 -->
-							      <v-tooltip max-width="300" top class="fixed-bottom">
-											<template v-slot:activator="{ on, attrs }">
-											  <div v-bind="attrs" v-on="on">
-											    <p class="content m-3">{{ karte.user.username }}</p>
-											  </div>
-											</template>
-											<span>{{ karte.user.username }}</span>
-							      </v-tooltip>							      
-						      </v-col>
-					      </v-row>
+    <v-row>
+      <v-spacer></v-spacer>
+      <v-col
+        v-for="karte in kartes"
+        :key="karte.id"
+        cols="12"
+        sm="6"
+        md="4"
+        @click="showKarte = karte"
+      >
+        <v-card :ripple="true" class="btn text-left" :loading="loading">
+          <v-card-actions class="white justify-center d-block">
+            <v-img
+              max-width="300"
+              class="mx-auto my-2 rounded-xl"
+              contain
+              :src="$karte(karte, authUser.username) + karte.image"
+              v-if="karte.image"
+            ></v-img>
 
-					      <!-- 活動内容 -->
-					      <v-tooltip max-width="300" top>
-									<template v-slot:activator="{ on, attrs }">
-									  <div v-bind="attrs" v-on="on">
-									    <p class="content text-truncate">{{ karte.body }}</p>
-									  </div>
-									</template>
-									<span>{{ karte.body }}</span>
-					      </v-tooltip>				      
+            <v-row align="center">
+              <v-col class="d-flex" cols="4" sm="2" md="10">
+                <!-- ユーザーアイコン -->
+                <v-avatar>
+                  <v-img :src="$storage('icon') + authUser.icon" contain max-width="50"> </v-img>
+                </v-avatar>
+                <!-- ユーザー名 -->
+                <v-tooltip max-width="300" top class="fixed-bottom">
+                  <template v-slot:activator="{ on, attrs }">
+                    <div v-bind="attrs" v-on="on">
+                      <p class="content m-3">{{ karte.user.username }}</p>
+                    </div>
+                  </template>
+                  <span>{{ karte.user.username }}</span>
+                </v-tooltip>
+              </v-col>
+            </v-row>
 
-					      <!-- 活動時間 -->
-								<p class="content">{{ karte.activity_time.slice(0, 5) }}</p>
+            <!-- 活動内容 -->
+            <v-tooltip max-width="300" top>
+              <template v-slot:activator="{ on, attrs }">
+                <div v-bind="attrs" v-on="on">
+                  <p class="content text-truncate">{{ karte.body }}</p>
+                </div>
+              </template>
+              <span>{{ karte.body }}</span>
+            </v-tooltip>
 
-					      <v-tooltip max-width="300" top>
-									<template v-slot:activator="{ on, attrs }">
-									  <div v-bind="attrs" v-on="on" v-if="karte.achieve">
-									    <p class="content text-truncate">{{ karte.achieve }}</p>
-									  </div>
+            <!-- 活動時間 -->
+            <p class="content">{{ karte.activity_time.slice(0, 5) }}</p>
 
-									  <div v-else>
-									    <p class="content">未入力</p>
-									  </div>
-									</template>
-									<span>{{ karte.achieve }}</span>
-					      </v-tooltip>				      
+            <v-tooltip max-width="300" top>
+              <template v-slot:activator="{ on, attrs }">
+                <div v-bind="attrs" v-on="on" v-if="karte.achieve">
+                  <p class="content text-truncate">{{ karte.achieve }}</p>
+                </div>
 
-					      <!-- 次の課題 -->
-					      <v-tooltip max-width="300" top>
-									<template v-slot:activator="{ on, attrs }">
-									  <div v-bind="attrs" v-on="on" v-if="karte.challenge">
-									    <p class="content text-truncate">{{ karte.challenge }}</p>
-									  </div>
+                <div v-else>
+                  <p class="content">未入力</p>
+                </div>
+              </template>
+              <span>{{ karte.achieve }}</span>
+            </v-tooltip>
 
-									  <div v-else>
-									    <p class="content">未入力</p>
-									  </div>
-									</template>
-									<span>{{ karte.challenge }}</span>
-					      </v-tooltip>
+            <!-- 次の課題 -->
+            <v-tooltip max-width="300" top>
+              <template v-slot:activator="{ on, attrs }">
+                <div v-bind="attrs" v-on="on" v-if="karte.challenge">
+                  <p class="content text-truncate">{{ karte.challenge }}</p>
+                </div>
 
-					      <!-- 参考文献 -->
-					      <v-tooltip max-width="300" top>
-									<template v-slot:activator="{ on, attrs }">
-									  <div v-bind="attrs" v-on="on" v-if="karte.reference">
-									    <p class="content text-truncate">{{ karte.reference }}</p>
-									  </div>
+                <div v-else>
+                  <p class="content">未入力</p>
+                </div>
+              </template>
+              <span>{{ karte.challenge }}</span>
+            </v-tooltip>
 
-									  <div v-else>
-									    <p class="content">未入力</p>
-									  </div>
-									</template>
-									<span>{{ karte.reference }}</span>
-					      </v-tooltip>				      
+            <!-- 参考文献 -->
+            <v-tooltip max-width="300" top>
+              <template v-slot:activator="{ on, attrs }">
+                <div v-bind="attrs" v-on="on" v-if="karte.reference">
+                  <p class="content text-truncate">{{ karte.reference }}</p>
+                </div>
 
-						    <!-- タグ -->
-					      <v-tooltip max-width="300" top>
-									<template v-slot:activator="{ on, attrs }">
-									  <div v-bind="attrs" v-on="on" v-if="karte.tags.length">
-											<v-chip class="ma-2" v-for="tag in karte.tags" :key="tag.id" :value="tag.id">
-											  {{ tag.name }}
-											</v-chip>
-									  </div>
+                <div v-else>
+                  <p class="content">未入力</p>
+                </div>
+              </template>
+              <span>{{ karte.reference }}</span>
+            </v-tooltip>
 
-									  <div v-else>
-									    <p class="content">タグなし</p>
-									  </div>
-									</template>
-									<v-chip class="ma-2" v-for="tag in karte.tags" :key="tag.id" :value="tag.id">
-									  {{ tag.name }}
-									</v-chip>
-					      </v-tooltip>
+            <!-- タグ -->
+            <v-tooltip max-width="300" top>
+              <template v-slot:activator="{ on, attrs }">
+                <div v-bind="attrs" v-on="on" v-if="karte.tags.length">
+                  <v-chip class="ma-2" v-for="tag in karte.tags" :key="tag.id" :value="tag.id">
+                    {{ tag.name }}
+                  </v-chip>
+                </div>
 
-						  </v-card-actions>
-					  </v-card>
-				  </v-col>
-			  </v-row>
+                <div v-else>
+                  <p class="content">タグなし</p>
+                </div>
+              </template>
+              <v-chip class="ma-2" v-for="tag in karte.tags" :key="tag.id" :value="tag.id">
+                {{ tag.name }}
+              </v-chip>
+            </v-tooltip>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
 
-		  </v-card>
-
-		  <v-card max-width="1080" tile class="ma-5" :loading="loading">
-      <v-simple-table headers-length:5>
-	      <template v-slot:default>
-          <thead>
-            <tr>
-              <th class="text-left">画像</th>
-              <th class="text-left">活動内容</th>
-              <th class="text-left">活動時間</th>
-              <th class="text-left">達成したこと</th>
-              <th class="text-left">次の課題</th>
-              <th class="text-left">参考文献</th>
-              <th class="text-left">タグ</th>
-            </tr>
-          </thead>
-				  <tbody>
-					  <tr v-for="karte in kartes" :key="kartes.id" @click="showKarte = karte">
-              <v-tooltip max-width="300" top>
-                <template v-slot:activator="{ on, attrs }">
-                  <td v-bind="attrs" v-on="on">
-                    <p class="content">{{ karte.user.username }}</p>
-                  </td>
-                </template>
-                <span>{{ karte.user.username }}</span>
-              </v-tooltip>
-
-              <td>
-                <v-img
-                  max-width="120"
-                  class="mx-auto my-2"
-                  contain
-                  :src="$karte(karte, authUser.username) + karte.image"
-                  v-if="karte.image"
-                />
-
-                <v-sheet
-                  color="grey lighten-2"
-                  width="120"
-                  height="120"
-                  class="mx-auto my-2"
-                  v-else
-                ></v-sheet>
-              </td>
-
-              <!-- 活動内容 -->
-              <v-tooltip max-width="300" top>
-                <template v-slot:activator="{ on, attrs }">
-                  <td v-bind="attrs" v-on="on">
-                    <p class="content">{{ karte.body }}</p>
-                  </td>
-                </template>
-                <span>{{ karte.body }}</span>
-              </v-tooltip>
-
-              <!-- 活動時間 -->
-              <td>
-                <p class="content">{{ karte.activity_time.slice(0, 5) }}</p>
-              </td>
-
-              <!-- 達成したこと -->
-              <v-tooltip max-width="300" top>
-                <template v-slot:activator="{ on, attrs }">
-                  <td v-bind="attrs" v-on="on" v-if="karte.achieve">
-                    <p class="content">{{ karte.achieve }}</p>
-                  </td>
-
-                  <td v-else>
-                    <p class="content">未入力</p>
-                  </td>
-                </template>
-                <span>{{ karte.achieve }}</span>
-              </v-tooltip>
-
-              <!-- 次の課題 -->
-              <v-tooltip max-width="300" top>
-                <template v-slot:activator="{ on, attrs }">
-                  <td v-bind="attrs" v-on="on" v-if="karte.challenge">
-                    <p class="content">{{ karte.challenge }}</p>
-                  </td>
-
-                  <td v-else>
-                    <p class="content">未入力</p>
-                  </td>
-                </template>
-                <span>{{ karte.challenge }}</span>
-              </v-tooltip>
-
-              <!-- 参考文献 -->
-              <v-tooltip max-width="300" top>
-                <template v-slot:activator="{ on, attrs }">
-                  <td v-bind="attrs" v-on="on" v-if="karte.reference">
-                    <p class="content">{{ karte.reference }}</p>
-                  </td>
-
-                  <td v-else>
-                    <p class="content">未入力</p>
-                  </td>
-                </template>
-                <span>{{ karte.reference }}</span>
-              </v-tooltip>
-
-              <!-- タグ -->
-              <v-tooltip max-width="300" top>
-                <template v-slot:activator="{ on, attrs }">
-                  <td v-bind="attrs" v-on="on" v-if="karte.tags.length">
-                    <p class="content">タグ{{ karte.tags.length }}個</p>
-                  </td>
-
-                  <td v-else>
-                    <p class="content">タグなし</p>
-                  </td>
-                </template>
-
-                <v-chip class="ma-2" v-for="tag in karte.tags" :key="tag.id" :value="tag.id">
-                  {{ tag.name }}
-                </v-chip>
-              </v-tooltip>
-					  </tr>
-				  </tbody>
-
-	      </template>
-	    </v-simple-table>
-		  </v-card>
-
-	    <KarteDialog :karte="showKarte" @close="showKarte = $event" />
-	  </v-container>
+    <KarteDialog :karte="showKarte" @close="showKarte = $event" />
+  </v-container>
 </template>
 
 <script>
 import KarteDialog from '@/components/mypage/KarteDialog';
 
 export default {
-	data() {
-		return {
+  data() {
+    return {
       loading: false, // ローディング制御
-			kartes: null,    //カルテ一覧
+      kartes: null, //カルテ一覧
       showKarte: null, // 詳細を表示するカルテ
-		}
-	},
-	components: {
-		KarteDialog
-	},
-	async created() {
-		this.loading = true;
+    };
+  },
+  components: {
+    KarteDialog,
+  },
+  async created() {
+    this.loading = true;
 
-		var response = await axios.get('/api/kartes');
-		this.kartes = response.data;
-		console.log(this.kartes);
+    var response = await axios.get('/api/kartes');
+    this.kartes = response.data;
+    console.log(this.kartes);
 
-		this.loading = false;
-	},
+    this.loading = false;
+  },
   computed: {
     authUser() {
       return this.$store.getters['auth/user'];
@@ -279,6 +152,22 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+h1 {
+  position: relative;
+  padding: 1rem 2rem;
+  border-bottom: 6px solid #094;
+
+  &:before {
+    position: absolute;
+    bottom: -6px;
+    left: 0;
+    width: 20%;
+    height: 6px;
+    content: '';
+    background: #00cc5b;
+  }
+}
+
 tbody {
   tr {
     cursor: pointer;
