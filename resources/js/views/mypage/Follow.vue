6992e18ea19a2e67137c6ebf7c53acc0f8cb17d0
tkknot <67575359+tkknot@users.noreply.github.com>
2021-04-07 09:04:12 +0900

    フォロー一覧、一部完成

--- resources/js/views/mypage/Follow.vue
+++ resources/js/views/mypage/Follow.vue
@@ -1,7 +1,62 @@
 <template>
-  <p>フォロー一覧</p>
+  <div>
+    <p>フォロー一覧</p>
+    <v-list v-if="show === 'follow' && followers.length">
+      <v-list-item
+        v-for="follower in followers"
+        :key="follower.id"
+        @click="showProfile(follower.username)"
+      >
+        <v-list-item-avatar>
+          <v-img :src="$storage('icon') + follower.icon"></v-img>
+        </v-list-item-avatar>
+
+        <v-list-item-content>
+          <v-list-item-title>
+            {{ follower.handlename }}
+            <small>@{{ follower.username }}</small>
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            {{ follower.introduction || '自己紹介が入力されていません。' }}
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    username: String, // 表示するユーザー名
+  },
+  data() {
+    return {
+      user: null,
+      show: null, //フォローの表示
+      followers: [], //フォロー一覧
+      profile: {
+        dialog: false, // 追加ダイアログ制御
+        username: null, // 表示するユーザー名
+      },
+    };
+  },
+  mounted() {
+    this.showFollows();
+  },
+  methods: {
+    showFollows: async function () {
+      //ユーザーデータの取得
+      // usernameが取れれば勝ち
+      let response = await axios.get('/api/users/' + this.username);
+      this.user = response.data;
+      console.log(this.user);
+
+      // フォロー一覧の表示
+      let responseFollow = await axios.get('/api/users/' + this.user.id + '/follows');
+      this.followers = responseFollow.data;
+      this.show = 'follow';
+    },
+  },
+};
 </script>
