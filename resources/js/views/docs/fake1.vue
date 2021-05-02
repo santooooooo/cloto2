a8e2440bb2d78791bf53c230942409ff8ac792ce
tkknot <67575359+tkknot@users.noreply.github.com>
2021-05-02 22:47:18 +0900

    #73 fake1の名でダミーファイルの作成

--- resources/js/views/docs/fake1.vue
+++ resources/js/views/docs/fake1.vue
@@ -9,16 +9,16 @@
           入門期<v-icon class="ml-4">mdi-chevron-down</v-icon>
         </h1>
         <h2 class="text-h6 font-weight-bolder">
-          野菜炒めを作るよ<v-icon class="ml-4 content-btn">mdi-chevron-down</v-icon>
+          Class 0<v-icon class="ml-4 content-btn">mdi-chevron-down</v-icon>
         </h2>
         <div class="pl-2">
           <!-- 目次 -->
           <ol>
-            <li class="my-6 grey--text" v-scroll-to="{ el: '#buy', offset: -80 }">食材を買う</li>
+            <li class="my-6 grey--text" v-scroll-to="{ el: '#buy', offset: -80 }">0-1.</li>
             <li class="my-6 grey--text" v-scroll-to="{ el: '#suggest', offset: -80 }">
-              おすすめの食材
+              0-2.
             </li>
-            <li class="my-6 grey--text" v-scroll-to="{ el: '#cut', offset: -80 }">食材を切る</li>
+            <li class="my-6 grey--text" v-scroll-to="{ el: '#cut', offset: -80 }">0-3</li>
           </ol>
         </div>
       </v-col>
@@ -27,32 +27,32 @@
         <!-- タイトル -->
         <div class="grey lighten-2 mb-8 pa-4">
           <h1 class="text-h4 font-weight-bold">{{ title }}</h1>
-          <p class="text-body-2 mx-6 mt-6 mb-0">塩コショウが効いた本格派</p>
+          <p class="text-body-2 mx-6 mt-6 mb-0">ダミー</p>
         </div>
 
         <!-- コンテンツ1 -->
         <v-container fluid class="mb-12" id="buy">
           <h4 class="text-h4 font-weight-bold text-left-border mb-10">
-            <span class="pl-3 grey lighten-3">食材を買う</span>
+            <span class="pl-3 grey lighten-3">ダミー</span>
           </h4>
 
-          <p class="mb-8">何を買おうかな</p>
+          <p class="mb-8">ダミー</p>
           <p class="mb-8">
-            人参、キャベツ、人参、キャベツ、人参、キャベツ、人参、キャベツ、人参、キャベツ、人参、キャベツ、人参、キャベツ、人参、キャベツ、人参、キャベツ、人参、キャベツ、人参、キャベツ、人参、キャベツ、
+ダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミー
           </p>
-          <p>こんなもんかな</p>
+          <p>ダミー</p>
         </v-container>
 
         <!-- コンテンツ2 -->
         <v-container fluid class="mb-12" id="suggest">
           <h4 class="text-h4 font-weight-bold text-left-border mb-10">
-            <span class="pl-3 grey lighten-3">おすすめの食材</span>
+            <span class="pl-3 grey lighten-3">ダミー</span>
           </h4>
 
-          <h5 class="text-h5 font-weight-bold mb-8">1.岩塩</h5>
+          <h5 class="text-h5 font-weight-bold mb-8">ダミー</h5>
           <v-row>
             <v-col cols="6">
-              <p class="mb-8">普通の塩には戻れない</p>
+              <p class="mb-8">ダミー</p>
             </v-col>
             <v-col cols="6">
               <v-img
@@ -64,10 +64,10 @@
             </v-col>
           </v-row>
 
-          <h5 class="text-h5 font-weight-bold mb-8">2.おいしい水</h5>
+          <h5 class="text-h5 font-weight-bold mb-8">ダミー</h5>
           <v-row>
             <v-col cols="6">
-              <p class="mb-8">普通の水には戻れない</p>
+              <p class="mb-8">ダミー</p>
             </v-col>
             <v-col cols="6">
               <v-img
@@ -83,18 +83,18 @@
         <!-- コンテンツ3 -->
         <v-container fluid class="mb-12" id="cut">
           <h4 class="text-h4 font-weight-bold text-left-border mb-10">
-            <span class="pl-3 grey lighten-3">食材を切る</span>
+            <span class="pl-3 grey lighten-3">ダミー</span>
           </h4>
 
-          <p class="mb-8">何から切る？</p>
-          <p class="mb-8">人参、と見せかけてキャベツ</p>
-          <p>どっちゃでもええ</p>
+          <p class="mb-8">ダミー</p>
+          <p class="mb-8">ダミー</p>
+          <p>ダミー</p>
         </v-container>
 
         <!-- 次へ進むボタン -->
         <v-col class="text-center">
           <!-- ルーティング箇所 -->
-          <v-btn color="blue" class="text-white" to="">完了（次へ進む）</v-btn>
+          <v-btn color="blue" class="text-white" to="/docs/senisaki">完了（次へ進む）</v-btn>
         </v-col>
       </v-col>
     </v-row>
