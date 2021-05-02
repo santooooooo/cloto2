3ad25e90c90c797235a0fd493c5dd7a52c99d5f5
santooooooo <santo.shunsuke@gmail.com>
2021-05-02 13:15:53 +0000

    #49 change descriptions in resouces/js/components/room/Call.vue and package-lock.json

--- resources/js/components/room/Call.vue
+++ resources/js/components/room/Call.vue
@@ -1285,19 +1285,19 @@ export default {
 
     // エラー発生時のイベント
     Vue.config.errorHandler = (error) => {
-      console.log(error);
+      //console.log(error);
       this.errorEvent('エラーが発生しました。再読み込みしてください。');
     };
 
     // エラー発生時のイベント
     window.addEventListener('error', (error) => {
-      console.log(error);
+      //console.log(error);
       this.errorEvent('エラーが発生しました。再読み込みしてください。');
     });
 
     // エラー発生時のイベント
     window.addEventListener('unhandledrejection', (error) => {
-      console.log(error);
+      //console.log(error);
       this.errorEvent('エラーが発生しました。再読み込みしてください。');
     });
 
