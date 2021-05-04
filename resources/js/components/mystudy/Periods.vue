fc89a5de0d8ebad549e28ccb44bd84ced06c3c2b
tkknot <67575359+tkknot@users.noreply.github.com>
2021-05-05 08:58:54 +0900

    #73 一つだけ仮データの設定 状態によって色が変わる

--- resources/js/components/mystudy/Periods.vue
+++ resources/js/components/mystudy/Periods.vue
@@ -1,42 +1,43 @@
 <template>
   <v-list nav dense>
     <v-list-item-group color="#f6bf00">
-      <v-list-item class="mb-4 green">
-        <v-list-item-content>
-          <v-list-item-title class="text-body-1 font-weight-bold grey--text">
-            入門期<span class="pl-2" style="font-size: 0.6rem">解放済み</span>
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
 
-      <v-list-item class="mb-4 blue">
-        <v-list-item-content>
-          <v-list-item-title class="text-body-1 font-weight-bold white--text accent-1">
-            素人期<span class="pl-2" style="font-size: 0.6rem">取り組み中</span>
+      <v-list-item class="mb-4">
+        <v-list-item-content id="enter" class="pl-1">
+          <v-list-item-title class="text-body-1 font-weight-bold accent-1">
+            入門期<span class="pl-2" id="enter-text"></span>
           </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
 
       <v-list-item class="mb-4">
-        <v-list-item-content>
-          <v-list-item-title class="text-body-1 font-weight-bold grey--text accent-1">
-            苦労人前期<span class="pl-2" style="font-size: 0.6rem">未解放</span>
+        <v-list-item-content class="pl-1">
+          <v-list-item-title class="text-body-1 font-weight-bold accent-1">
+            素人期<span class="pl-2">取り組み中</span>
           </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
 
       <v-list-item class="mb-4">
-        <v-list-item-content>
-          <v-list-item-title class="text-body-1 font-weight-bold grey--text accent-1">
-            苦労人後期<span class="pl-2" style="font-size: 0.6rem">未解放</span>
+        <v-list-item-content class="pl-1">
+          <v-list-item-title class="text-body-1 font-weight-bold accent-1">
+            苦労人前期<span class="pl-2">未解放</span>
           </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
 
       <v-list-item class="mb-4">
-        <v-list-item-content>
-          <v-list-item-title class="text-body-1 font-weight-bold grey--text accent-1">
-            玄人期<span class="pl-2" style="font-size: 0.6rem">未解放</span>
+        <v-list-item-content class="pl-1">
+          <v-list-item-title class="text-body-1 font-weight-bold accent-1">
+            苦労人後期<span class="pl-2">未解放</span>
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-list-item class="mb-4">
+        <v-list-item-content class="pl-1">
+          <v-list-item-title class="text-body-1 font-weight-bold accent-1">
+            玄人期<span class="pl-2">未解放</span>
           </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
@@ -53,4 +54,42 @@
     text-decoration: none;
   }
 }
+
+span{
+  font-size: 0.6rem;
+}
 </style>
+
+<script>
+export default {
+  data(){
+    return {
+      progress: {
+      enter: 2,
+      begin: 1,
+      cloFirst: 0,
+      cloSecond: 0,
+      cloto: 0
+      }
+    }
+  },
+  mounted() {
+    let enter = document.getElementById('enter')
+    let enterText = document.getElementById('enter-text')
+    switch (this.progress.enter) {
+      case 2:
+        enter.classList.add('white--text', 'green')
+        enterText.innerHTML = "解放済み"
+        break;
+      case 1:
+        enter.classList.add('white--text', 'blue')
+        enterText.innerHTML = "取り組み中"
+        break;
+      default:
+        enter.classList.add('grey--text')
+        enterText.innerHTML = "未解放"
+        break;
+    }
+  },
+}
+</script>
