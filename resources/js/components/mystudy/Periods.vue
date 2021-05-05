a4e931a932d1ab0b17f69912d7b4badbac9842dc
tkknot <67575359+tkknot@users.noreply.github.com>
2021-05-05 15:06:18 +0900

    #73 switch文でとりあえずの状態管理、多分もっと良い書き方があるはず

--- resources/js/components/mystudy/Periods.vue
+++ resources/js/components/mystudy/Periods.vue
@@ -11,33 +11,33 @@
       </v-list-item>
 
       <v-list-item class="mb-4">
-        <v-list-item-content class="pl-1">
+        <v-list-item-content id="begin" class="pl-1">
           <v-list-item-title class="text-body-1 font-weight-bold accent-1">
-            素人期<span class="pl-2">取り組み中</span>
+            素人期<span class="pl-2" id="begin-text"></span>
           </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
 
       <v-list-item class="mb-4">
-        <v-list-item-content class="pl-1">
+        <v-list-item-content id="hard-one" class="pl-1">
           <v-list-item-title class="text-body-1 font-weight-bold accent-1">
-            苦労人前期<span class="pl-2">未解放</span>
+            苦労人前期<span class="pl-2" id="hard-one-text"></span>
           </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
 
       <v-list-item class="mb-4">
-        <v-list-item-content class="pl-1">
+        <v-list-item-content id="hard-two" class="pl-1">
           <v-list-item-title class="text-body-1 font-weight-bold accent-1">
-            苦労人後期<span class="pl-2">未解放</span>
+            苦労人後期<span class="pl-2" id="hard-two-text"></span>
           </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
 
       <v-list-item class="mb-4">
-        <v-list-item-content class="pl-1">
+        <v-list-item-content id="master" class="pl-1">
           <v-list-item-title class="text-body-1 font-weight-bold accent-1">
-            玄人期<span class="pl-2">未解放</span>
+            玄人期<span class="pl-2" id="master-text"></span>
           </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
@@ -76,6 +76,19 @@ export default {
   mounted() {
     let enter = document.getElementById('enter')
     let enterText = document.getElementById('enter-text')
+
+    let begin = document.getElementById('begin')
+    let beginText = document.getElementById('begin-text')
+
+    let hardOne = document.getElementById('hard-one')
+    let hardOneText = document.getElementById('hard-one-text')
+
+    let hardTwo = document.getElementById('hard-two')
+    let hardTwoText = document.getElementById('hard-two-text')
+
+    let master = document.getElementById('master')
+    let masterText = document.getElementById('master-text')
+
     switch (this.progress.enter) {
       case 2:
         enter.classList.add('white--text', 'green')
@@ -88,6 +101,62 @@ export default {
       default:
         enter.classList.add('grey--text')
         enterText.innerHTML = "未解放"
+        break;
+    }
+    switch (this.progress.begin) {
+      case 2:
+        begin.classList.add('white--text', 'green')
+        beginText.innerHTML = "解放済み"
+        break;
+      case 1:
+        begin.classList.add('white--text', 'blue')
+        beginText.innerHTML = "取り組み中"
+        break;
+      default:
+        begin.classList.add('grey--text')
+        beginText.innerHTML = "未解放"
+        break;
+    }
+    switch (this.progress.hardOne) {
+      case 2:
+        hardOne.classList.add('white--text', 'green')
+        hardOneText.innerHTML = "解放済み"
+        break;
+      case 1:
+        hardOne.classList.add('white--text', 'blue')
+        hardOneText.innerHTML = "取り組み中"
+        break;
+      default:
+        hardOne.classList.add('grey--text')
+        hardOneText.innerHTML = "未解放"
+        break;
+    }
+    switch (this.progress.hardTwo) {
+      case 2:
+        hardTwo.classList.add('white--text', 'green')
+        hardTwoText.innerHTML = "解放済み"
+        break;
+      case 1:
+        hardTwo.classList.add('white--text', 'blue')
+        hardTwoText.innerHTML = "取り組み中"
+        break;
+      default:
+        hardTwo.classList.add('grey--text')
+        hardTwoText.innerHTML = "未解放"
+        break;
+    }
+    switch (this.progress.master) {
+      case 2:
+        master.classList.add('white--text', 'green')
+        masterText.innerHTML = "解放済み"
+        break;
+      case 1:
+        master.classList.add('white--text', 'blue')
+        masterText.innerHTML = "取り組み中"
+        break;
+      default:
+        master.classList.add('grey--text')
+        masterText.innerHTML = "未解放"
         break;
     }
   },
