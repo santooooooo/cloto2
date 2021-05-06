24d0569bc9e1da1bf61edcea7ec4e47bf70a7955
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-05-06 18:57:37 +0900

    #80 デザインの修正

--- resources/js/views/mystudy/Mystudy.vue
+++ resources/js/views/mystudy/Mystudy.vue
@@ -1,7 +1,7 @@
 <template>
   <v-layout id="mystudy">
     <Drawer />
-    <v-flex>
+    <v-flex class="overflow-y-auto">
       <!-- 強制的にコンポーネントを初期化（変数の初期化） -->
       <router-view :key="$route.name" />
     </v-flex>
