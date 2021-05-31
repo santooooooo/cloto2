91b5b2feaba9109cb261e1d23c313e5a96b01c29
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-05-31 23:58:14 +0900

    #73 整形

--- resources/js/views/mystudy/docs/javascript/Class10.vue
+++ resources/js/views/mystudy/docs/javascript/Class10.vue
@@ -1,81 +1,64 @@
 <template>
-  <v-container fluid class="mt-4">
-    <v-row>
-      <!-- 目次 -->
-      <ContentTable :contents="contents" />
+  <v-container fluid class="mt-4" id="content">
+    <!-- タイトル -->
+    <div class="mb-4 pa-4">
+      <h1 class="text-h4 text-center mb-12">{{ title }}</h1>
+      <p class="text-body-2 mx-6">このコンテンツの説明</p>
+    </div>
 
-      <v-col cols="8" id="content">
-        <!-- タイトル -->
-        <div class="mb-4 pa-4">
-          <h1 class="text-h4 text-center mb-12">{{ title }}</h1>
-          <p class="text-body-2 mx-6">このコンテンツの説明</p>
-        </div>
+    <!-- コンテンツ1 -->
+    <v-container fluid class="mb-12" id="buy">
+      <h2 class="text-h4 font-weight-bold mb-10">
+        <span class="pl-3 grey lighten-3">ダミー1</span>
+      </h2>
 
-        <!-- コンテンツ1 -->
-        <v-container fluid class="mb-12" id="buy">
-          <h2 class="text-h4 font-weight-bold mb-10">
-            <span class="pl-3 grey lighten-3">ダミー1</span>
-          </h2>
+      <p class="mb-8">ダミー</p>
+      <p class="mb-8">
+        ダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミー
+      </p>
+      <p>ダミー</p>
+    </v-container>
 
+    <!-- コンテンツ2 -->
+    <v-container fluid class="mb-12" id="suggest">
+      <h2 class="text-h4 font-weight-bold mb-10">
+        <span class="pl-3 grey lighten-3">ダミー2</span>
+      </h2>
+
+      <h5 class="text-h5 font-weight-bold mb-8">ダミー</h5>
+      <v-row>
+        <v-col cols="6">
           <p class="mb-8">ダミー</p>
-          <p class="mb-8">
-            ダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミー
-          </p>
-          <p>ダミー</p>
-        </v-container>
+        </v-col>
+        <v-col cols="6">
+          <v-img max-width="90%" max-height="60vh" :src="filePath + '1.png'" class="mb-10"></v-img>
+        </v-col>
+      </v-row>
 
-        <!-- コンテンツ2 -->
-        <v-container fluid class="mb-12" id="suggest">
-          <h2 class="text-h4 font-weight-bold mb-10">
-            <span class="pl-3 grey lighten-3">ダミー2</span>
-          </h2>
-
-          <h5 class="text-h5 font-weight-bold mb-8">ダミー</h5>
-          <v-row>
-            <v-col cols="6">
-              <p class="mb-8">ダミー</p>
-            </v-col>
-            <v-col cols="6">
-              <v-img
-                max-width="90%"
-                max-height="60vh"
-                :src="filePath + '1.png'"
-                class="mb-10"
-              ></v-img>
-            </v-col>
-          </v-row>
-
-          <h5 class="text-h5 font-weight-bold mb-8">ダミー</h5>
-          <v-row>
-            <v-col cols="6">
-              <p class="mb-8">ダミー</p>
-            </v-col>
-            <v-col cols="6">
-              <v-img
-                max-width="90%"
-                max-height="60vh"
-                :src="filePath + '2.png'"
-                class="mb-10"
-              ></v-img>
-            </v-col>
-          </v-row>
-        </v-container>
-
-        <!-- コンテンツ3 -->
-        <v-container fluid class="mb-12" id="cut">
-          <h2 class="text-h4 font-weight-bold mb-10">
-            <span class="pl-3 grey lighten-3">ダミー3</span>
-          </h2>
-
+      <h5 class="text-h5 font-weight-bold mb-8">ダミー</h5>
+      <v-row>
+        <v-col cols="6">
           <p class="mb-8">ダミー</p>
-          <p class="mb-8">ダミー</p>
-          <p>ダミー</p>
-        </v-container>
+        </v-col>
+        <v-col cols="6">
+          <v-img max-width="90%" max-height="60vh" :src="filePath + '2.png'" class="mb-10"></v-img>
+        </v-col>
+      </v-row>
+    </v-container>
 
-        <v-row justify="center" class="mb-4">
-          <v-btn dark color="primary" @click="$emit('next')">完了（次へ進む）</v-btn>
-        </v-row>
-      </v-col>
+    <!-- コンテンツ3 -->
+    <v-container fluid class="mb-12" id="cut">
+      <h2 class="text-h4 font-weight-bold mb-10">
+        <span class="pl-3 grey lighten-3">ダミー3</span>
+      </h2>
+
+      <p class="mb-8">ダミー</p>
+      <p class="mb-8">ダミー</p>
+      <p>ダミー</p>
+    </v-container>
+
+    <v-row justify="center" class="mb-4">
+      <v-btn dark color="primary" @click="$emit('next')">完了（次へ進む）</v-btn>
     </v-row>
   </v-container>
 </template>
