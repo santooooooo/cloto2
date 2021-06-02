1a08d1cc3f1421ba4d5e61113165119cb002b830
tkknot <67575359+tkknot@users.noreply.github.com>
2021-06-02 21:38:53 +0900

    #73 clearページ完了

--- resources/js/views/mystudy/docs/javascript/Clear.vue
+++ resources/js/views/mystudy/docs/javascript/Clear.vue
@@ -1,58 +1,16 @@
 <template>
   <v-container fluid id="content">
-    <!-- タイトル -->
-    <div class="mb-12 pa-4">
-      <h1 class="text-h3 text-center">{{ title }}</h1>
-      <p class="text-body-2 mx-6">このコンテンツの説明</p>
+    <div class="pa-4" id="cleardesc">
+      <h1 class="text-h1 text-center">{{ title }}</h1>
     </div>
 
-    <!-- コンテンツ1 -->
-    <h2 class="text-h4 font-weight-bold">
-      <span>ダミー1</span>
-    </h2>
-    <v-container fluid>
-      <p class="mb-8">ダミー</p>
-      <p class="mb-8">
-        ダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミーダミー
-      </p>
-      <p>ダミー</p>
-    </v-container>
-
-    <!-- コンテンツ2 -->
-    <h2 class="text-h4 font-weight-bold">
-      <span>ダミー2</span>
-    </h2>
-    <v-container fluid>
-      <h5 class="text-h5 font-weight-bold mb-8">ダミー</h5>
-      <v-row>
-        <v-col cols="6">
-          <p class="mb-8">ダミー</p>
-        </v-col>
-        <v-col cols="6">
-          <v-img max-width="90%" max-height="60vh" :src="filePath + '1.png'" class="mb-10"></v-img>
-        </v-col>
-      </v-row>
-
-      <h5 class="text-h5 font-weight-bold mb-8">ダミー</h5>
-      <v-row>
-        <v-col cols="6">
-          <p class="mb-8">ダミー</p>
-        </v-col>
-        <v-col cols="6">
-          <v-img max-width="90%" max-height="60vh" :src="filePath + '2.png'" class="mb-10"></v-img>
-        </v-col>
-      </v-row>
-    </v-container>
-
-    <!-- コンテンツ3 -->
-    <h2 class="text-h4 font-weight-bold">
-      <span>ダミー3</span>
-    </h2>
-    <v-container fluid>
-      <p class="mb-8">ダミー</p>
-      <p class="mb-8">ダミー</p>
-      <p>ダミー</p>
-    </v-container>
+      <h2 class="font-weight-bold" style="display: none;">
+        <span>
+          
+        クリアおめでとう！！
+        </span>
+        
+        </h2>
 
     <v-row justify="center">
       <v-btn color="blue" class="text-white" :to="{ name: 'home' }">みんなを助けに行く</v-btn>
@@ -71,9 +29,35 @@ export default {
   },
   data() {
     return {
-      title: 'クリア', // コンテンツタイトル
+      title: 'CLEAR', // コンテンツタイトル
       filePath: this.$docs(this.$route.params.roadName, 'class' + Number(this.$route.params.class)), // コンテンツデータのパス
     };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+  @import '~/_variables';
+
+
+      #cleardesc {
+        background-color: #f6bf00;
+        color: #ffffff;
+        height: 80vh;
+        position: relative;
+        margin-bottom: 80px;
+        & > h1 {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translateY(-50%) translateX(-50%);
+        }
+      }
+      h2 {
+        span {
+          padding-left: 12px;
+          border-left: 6px solid $dark-blue;
+          background-color: #eeeeee;
+        }
+      }
+</style>
