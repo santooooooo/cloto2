2a2defc34a422cbeb614ecddf0a1e6b61407a446
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-07-20 00:37:08 +0900

    #61 bloadcast.blade.phpの作成

---
+++ app/Mail/BroadcastMail.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class BroadcastMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /** @var Array */
+    protected $content;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(array $content)
+    {
+        $this->content = $content;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->text('emails.broadcast')
+            ->to($this->content['to'], $this->content['to_name'])
+            ->from($this->content['from'], $this->content['from_name'])
+            ->subject($this->content['subject'])
+            ->with(['content' => $this->content]);
+    }
+}
