57c1fd10d22eba05b83014a1e31f0af9407dec00
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-07-20 14:36:28 +0900

    #61 メールアドレスの設定

--- app/Http/Controllers/API/BroadcastController.php
+++ app/Http/Controllers/API/BroadcastController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\BroadcastMail;
+use App\Models\User;
 use Exception;
 
 class BroadcastController extends Controller
@@ -19,18 +20,24 @@ class BroadcastController extends Controller
     public function broadcast(Request $request)
     {
         try {
-            Mail::send(new BroadcastMail([
-                'to' => $request->email,
-                'to_name' => $request->name,
-                'from' => config('mail.service.contact'),
-                'from_name' => config('app.name'),
-                'subject' => '【お問い合わせ受付完了】- ' . config('app.name'),
-                'body' => $request->body
-            ], 'user'));
+            $users = User::all();
+
+            foreach ($users as $user) {
+                if ($user->newsletter) {
+                    Mail::send(new BroadcastMail([
+                        'to' => $user->email,
+                        'to_name' => $user->handlename,
+                        'from' => config('mail.newsletter.address'),
+                        'from_name' => config('mail.newsletter.name'),
+                        'subject' => $request->subject,
+                        'body' => $request->body
+                    ]));
+                }
+            }
         } catch (Exception $e) {
-            return response()->json(['message' => 'エラーが発生しました。'], config('consts.status.INTERNAL_SERVER_ERROR'));
+            // メール送信時のエラーを無視，処理を続行する
         }
 
-        return response()->json(['message' => 'お問い合わせを送信しました。']);
+        return response()->json(['message' => 'ニュースレターを送信しました。']);
     }
 }
