0094edd52b402929fa2201160daa8e2d03d492a0
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-09-13 23:28:34 +0900

    #66 Questionモデルの作成

---
+++ app/Models/Question.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    protected $primaryKey = 'id';
+    protected $dates = ['created_at', 'updated_at'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'title', 'body'
+    ];
+
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = ['user'];
+
+    /**
+     * User モデルのリレーション
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User');
+    }
+
+    /**
+     * ユーザーデータの追加
+     *
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function getUserAttribute()
+    {
+        return $this->user()->first();
+    }
+}
---
+++ database/migrations/2021_09_13_232115_create_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use App\Database\Blueprint;
+use App\Facades\Schema;
+
+class CreateQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('questions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();        // ユーザーID
+            $table->string('title');        // タイトル
+            $table->text('body');           // 内容
+            $table->dateTimes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('questions');
+    }
+}
