9757e28d490d4e40733870f09ee490e1f662685c
a2h1r0 <atsuhiro.fujii.98@gmail.com>
2021-09-18 23:50:08 +0900

    #66 QuestionDialogの体裁調整

--- resources/js/components/commons/QuestionDialog.vue
+++ resources/js/components/commons/QuestionDialog.vue
@@ -8,22 +8,24 @@
           </v-btn>
         </v-row>
         <v-row justify="center">
-          <!-- コメント数 -->
+          <!-- 回答数 -->
           <span class="mx-3 small">
             <v-icon>mdi-message-text</v-icon>
-            {{ question.comments_count }}
-          </span>
-
-          <!-- いいね数 -->
-          <span class="mx-3 small">
-            <v-icon>mdi-heart</v-icon>
-            {{ question.favorites_count }}
+            {{ question.answers_count }}
           </span>
         </v-row>
 
         <v-row justify="center" class="mt-2">
-          <v-card rounded="xl" min-height="154" width="800" class="mb-4 text-left">
+          <p class="text-body-1">{{ question.title }}</p>
+
+          <v-card-text class="pa-2 white--text title font-weight-bold">質問内容</v-card-text>
+          <v-card rounded="xl" min-height="154" width="90%" class="mb-4 text-left">
             <pre class="px-6 py-2 text-body-1" v-html="$formatStr(question.body)"></pre>
+          </v-card>
+
+          <v-card-text class="pa-2 white--text title font-weight-bold">試したこと</v-card-text>
+          <v-card rounded="xl" min-height="154" width="90%" class="mb-4 text-left">
+            <pre class="px-6 py-2 text-body-1" v-html="$formatStr(question.tried)"></pre>
           </v-card>
         </v-row>
       </v-container>
@@ -32,7 +34,7 @@
       <CommentContainer
         item-type="question"
         :item-id="question.id"
-        :comments="question.comments"
+        :comments="question.answers"
         @update="getQuestion()"
       />
     </v-card>
